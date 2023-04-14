031766fc66316347b36b269339cc32f4eeaec5f3
ceolantir <songhoyeon16@gmail.com>
2023-04-14 19:56:17 +0700

    executable file of the module

--- fsspgov/main.py
+++ fsspgov/main.py
@@ -1,2 +1,17 @@
+import logging
+
+import extractions
+import csv_handler
+
+logging.basicConfig(level=logging.WARNING, filename="logs/log.log", filemode="w")
+
 if __name__ == "__main__":
-    pass
+    csv = csv_handler.CSVHandler()
+    input_data = csv.read_excel()
+
+    session = extractions.FsspData()
+    result = []
+    for row in input_data:
+        result.extend(session.start(row))
+
+    csv.write_excel(result)
